0d14d0eac0a887948b738d7752e7a739b215d6c6
NickLouis <nicktolmie86@gmail.com>
2022-02-16 19:21:54 +0000

    Updating further

--- src/main/java/streams/_Stream.java
+++ src/main/java/streams/_Stream.java
@@ -1,6 +1,11 @@
 package streams;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import streams._Stream.Gender;
+
 import static streams._Stream.Gender.*;
 
 public class _Stream {
@@ -11,7 +16,18 @@ public class _Stream {
 	
 
 	public static void main(String[] args) {
-		List <Person> people = List.of(new Person("Tom", MALE));
+		List <Person> people = List.of(new Person("Tom", MALE)
+									,new Person("Tommy", MALE)
+									,new Person("Tomina", FEMALE)
+									,new Person("Tomatha", FEMALE));
+		
+		
+		boolean tom= people.stream().allMatch(person -> person.name.contains("Tom"));
+		System.out.println("Toms? -> " + tom);
+		System.out.println("Raabss? -> "+ people.stream().anyMatch(person -> person.name.equals("Raab")));
+		
+		Set<Gender> genders = people.stream().map( person -> person.gender).collect(Collectors.toSet());
+		
 
 	}
 	
